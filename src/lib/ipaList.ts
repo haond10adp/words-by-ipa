2e191225355a095024546854d3012b992b3bd2f9
Hao <haond10adp@gmail.com>
2024-07-13 21:32:22 +0700

    u

--- src/lib/ipaList.ts
+++ src/lib/ipaList.ts
@@ -2,7 +2,7 @@ const ipaList = {
 	shortVowel: ['æ', 'ʌ', 'ʊ', 'ɒ', 'ə', 'ɪ', 'e'],
 	longVowel: ['iː', 'ɔː', 'uː', 'ɑː', '(r)'],
 	dConsonants: ['θ', 'ð', 'ʃ', 'ʒ', 'tʃ', 'dʒ'],
-	eConsonants: ['f', 'v', 's', 'z', 'h', 'p', 'b', 't', 'd', 'k', 'g', 'm', 'n', 'h']
+	eConsonants: ['f', 'v', 's', 'z', 'h', 'p', 'b', 't', 'd', 'k', 'ɡ', 'm', 'n', 'ŋ', 'h']
 };
 
 export default ipaList;
